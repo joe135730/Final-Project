7da76efb5256a0c916c36dcfbfce15f7c3dd47c6
Jocelyn Wang <wang.tzup@northeastern.edu>
2024-08-13 11:30:55 +0800

    Add comments for PeopleManager

--- Final Project/src/People/PeopleManager.java
+++ Final Project/src/People/PeopleManager.java
@@ -5,15 +5,31 @@ import java.util.List;
 
 import People.Person;
 
+/**
+ * The PeopleManager class is responsible for managing the collection of Person objects,
+ * segregating Person objects into separate lists based on their sex.
+ */
 public class PeopleManager {
+  // List for holding male persons
   private final List<Person> maleList;
+  // List for holding female persons
   private final List<Person> femaleList;
 
+  /**
+   * PeopleManager constructor that initializes the lists for male and female Persons.
+   */
   public PeopleManager() {
+    // Initialize maleList ArrayList for storing objects identified as male
     maleList = new ArrayList<>();
+    // Initialize femaleList ArrayList for storing objects identified as female
     femaleList = new ArrayList<>();
   }
 
+  /**
+   * Adds a Person object to the list based on the person's sex.
+   *
+   * @param person The Person object to be added
+   */
   public void addPerson(Person person) {
     if ("Male".equalsIgnoreCase(person.getSex())) {
       maleList.add(person);
@@ -22,10 +38,20 @@ public class PeopleManager {
     }
   }
 
+  /**
+   * Returns the list of all male Persons.
+   *
+   * @return A List of Person objects who are identified as male
+   */
   public List<Person> getMaleList() {
     return maleList;
   }
 
+  /**
+   * Returns the list of all female Persons.
+   *
+   * @return A List of Person objects who are identified as female
+   */
   public List<Person> getFemaleList() {
     return femaleList;
   }
