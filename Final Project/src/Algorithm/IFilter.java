5d940095d72455493d356c9ff5d15e828599b348
Jocelyn Wang <wang.tzup@northeastern.edu>
2024-08-13 11:37:47 +0800

    Add comments- IFilter

--- Final Project/src/Algorithm/IFilter.java
+++ Final Project/src/Algorithm/IFilter.java
@@ -4,7 +4,27 @@ import java.util.List;
 
 import People.Person;
 
+/**
+ * The IFilter interface defines methods for adding preferred candidates based on gender,
+ * providing a contract for implementing classes to handle gender-specific filtering of candidates.
+ */
 public interface IFilter {
-   void addPreferCandidatesMale(List<Person> males, List<Person> females);
-   void addPreferCandidatesFemale(List<Person> males, List<Person> females);
+
+  /**
+   * Adds male candidates from a given list of males and females to the preferred list.
+   * Implementing classes defines how males are selected and added.
+   *
+   * @param males   List of male candidates to be filtered or processed
+   * @param females List of female candidates, potentially for comparison
+   */
+  void addPreferCandidatesMale(List<Person> males, List<Person> females);
+
+  /**
+   * Adds female candidates from a given list of males and females to the preferred list.
+   * Implementing classes should define how females are selected and added.
+   *
+   * @param males   List of male candidates, potentially for comparison or exclusion
+   * @param females List of female candidates to be filtered or processed
+   */
+  void addPreferCandidatesFemale(List<Person> males, List<Person> females);
 }
